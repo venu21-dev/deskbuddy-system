67d08874199cd6143da76ae00db14e5cddf96b16
Venurshan Manivannan <venurshan.manivannan@gmail.com>
2026-04-21 00:38:01 +0200

    feat: configure SQLite database with EF Core in Program.cs

--- backend/DeskBuddy.Api/Program.cs
+++ backend/DeskBuddy.Api/Program.cs
@@ -1,8 +1,14 @@
+using DeskBuddy.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
